e584a9bd05cc1a0c8a81c1e1e639944792fccd38
Waleed Chayeb <waleedchayeb2@gmail.com>
2018-03-25 23:31:26 +0300

    all2

---
+++ SignalRChatApp/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNet.SignalR;
+using System.Linq; 
+
+namespace SignalRChatApp
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
---
+++ SignalRChatApp/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Owin;
+ 
+
+namespace SignalRChatApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
